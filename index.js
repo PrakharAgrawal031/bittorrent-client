0018734ca9269302ae712a6dd936781a9fb084f0
Prakhar <agrawalprakhar031@gmail.com>
2024-09-15 14:57:44 +0530

    add new torrent-parser.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const fs = require('fs');
 const bencode = require('bencode');
 const tracker = require('./src/tracker.js');
-
-const torrent = bencode.decode(fs.readFileSync('./bit1.torrent'));
+const torrentParser = require('./src/torrent-parser.js')
+const torrent = torrentParser.open('./bit3.torrent')
 
 tracker.getpeers(torrent, peers=>{
     console.log('list of peers: ', peers);
