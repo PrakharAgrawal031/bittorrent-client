ce4e2b2124c84870ad49e23efe98a22afd140d74
Prakhar <agrawalprakhar031@gmail.com>
2024-09-15 14:49:55 +0530

    add util.js

--- src/util.js
+++ src/util.js
@@ -6,7 +6,7 @@ let id = null;
 module.exports.genId = () => {
   if (!id) {
     id = crypto.randomBytes(20);
-    Buffer.from("-AT0001-").copy(id, 0);
+    Buffer.from("-IT0001-").copy(id, 0);
   }
   return id;
 };
