3a6c8cf6971343ce24b9b3f1add98075fbf3c941
Prakhar <agrawalprakhar031@gmail.com>
2024-09-16 23:34:50 +0530

    complete download.js

--- src/download.js
+++ src/download.js
@@ -8,12 +8,12 @@ const Buffer = require("buffer").Buffer;
 const tracker = require("./tracker.js");
 
 const main = (torrent, path) => {
-    tracker.getpeers(torrent, peers => {
-        const pieces = new Pieces(torrent);
-        const file = fs.openSync(path, 'w');
-        peers.forEach(peer => handlePeer(peer, torrent, pieces, file));
-    });
-}
+  tracker.getpeers(torrent, (peers) => {
+    const pieces = new Pieces(torrent);
+    const file = fs.openSync(path, "w");
+    peers.forEach((peer) => handlePeer(peer, torrent, pieces, file));
+  });
+};
 
 // function download(peer, torrent) {
 //   const socket = net.Socket();
@@ -28,21 +28,25 @@ const main = (torrent, path) => {
 // }
 
 //Using handlePeer instead of download
-function handlePeer( peer, torrent, pieces, file){
-    const socket = new net.Socket()
-    socket.on('error', err => {
-        console.log(`Error connecting to peer ${peer.ip}:${peer.port} - ${err.message}`);
-        socket.end();
-    });
+function handlePeer(peer, torrent, pieces, file) {
+  const socket = new net.Socket();
+  socket.on("error", (err) => {
+    console.log(
+      `Error connecting to peer ${peer.ip}:${peer.port} - ${err.message}`
+    );
+    socket.end();
+  });
 
-    socket.connect(peer.port, peer.ip, () => {
-        console.log(`Connected to peer ${peer.ip}:${peer.port}`)
-        socket.write(message.buildHandshake(torrent));
-    });
+  socket.connect(peer.port, peer.ip, () => {
+    console.log(`Connected to peer ${peer.ip}:${peer.port}`);
+    socket.write(message.buildHandshake(torrent));
+  });
 
-    const queue = new Queue(torrent);
-    onWholeMssg(socket, peer.ip, message => mssgHandler(message, socket, pieces, queue, torrent, file));
-};
+  const queue = new Queue(torrent);
+  onWholeMssg(socket, peer.ip, (message) =>
+    mssgHandler(message, socket, pieces, queue, torrent, file)
+  );
+}
 
 function onWholeMssg(socket, peer_ip, callback) {
   let savedBuffer = Buffer.alloc(0);
@@ -100,3 +104,75 @@ function isHandshake(message) {
     message.toString("utf8", 1, 20) === "BitTorrent Protocol"
   );
 }
+
+function chokeHandler(socket) {
+  socket.end();
+}
+
+function unchokeHandler(socket, pieces, queue) {
+  queue.choked = false;
+  requestPiece(socket, pieces, queue);
+}
+
+function haveHandler(socket, pieces, queue, payload) {
+  const pieceIndex = payload.readUInt32BE(0);
+  const queueEmpty = queue.length === 0;
+  queue.queue(pieceIndex);
+  if (queueEmpty) requestPiece(socket, pieces, queue);
+}
+
+function bitfieldHandler(socket, pieces, queue, payload) {
+  const queueEmpty = queue.length === 0;
+  payload.forEach((byte, i) => {
+    for (let j = 0; j < 8; j++) {
+      if (byte % 2) queue.queue(i * 8 + 7 - j);
+      byte = Math.floor(byte / 2);
+    }
+  });
+  if (queueEmpty) requestPiece(socket, pieces, queue);
+}
+
+function pieceHandler(socket, pieces, queue, torrent, file, payload) {
+  pieces.printPercentDone();
+  pieces.addRecieve(pieceResponse);
+
+  const offset =
+    pieceResponse.index * torrent.info[" piece length "] + pieceResponse.begin;
+  fs.writeFile(
+    file,
+    pieceResponse.block,
+    0,
+    pieceResponse.block.length,
+    offset,
+    (err) => {
+      if (err) console.log("Error writing to file:", err);
+    }
+  );
+
+  if (pieces.isDone()) {
+    console.log("Download Complete");
+    socket.end();
+    try {
+      fs.closeSync(file);
+    } catch (e) {
+      console.error(`Error closing file `, err);
+    }
+  } else {
+    requestPiece(socket, pieces, queue);
+  }
+}
+
+function requestPiece(socket, pieces, queue) {
+  if (queue.choked) return null;
+
+  while (queue.length()) {
+    const pieceBlock = queue.deque();
+    if (pieces.needed(pieceBlock)) {
+      socket.write(message.buildRequest(pieceBlock));
+      pieces.addRequested(pieceBlock);
+      break;
+    }
+  }
+}
+
+module.exports = main;
