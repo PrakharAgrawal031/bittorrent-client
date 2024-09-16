1ae260c1c8f814d98f1a447fae76cfd7405af917
Prakhar <agrawalprakhar031@gmail.com>
2024-09-16 22:11:04 +0530

    add onWholeMssg function to download.js

--- src/download.js
+++ src/download.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const net = require('net');
-const { Buffer } = require('buffer');
+const Buffer = require('buffer').Buffer;
 const tracker = require('./tracker.js');
 
 module.exports = torrent => {
@@ -21,4 +21,24 @@ function download(peer){
         //TODO: handle response
     })
 
+}
+
+function onWholeMssg(socket, peer_ip, callback){
+    let savedBuffer = Buffer.alloc(0)
+    let handshake = true;
+
+    socket.on('data', recvBuffer =>{
+        const mssgLength = () => handshake ? savedBuffer.readUInt8(0) + 49 : savedBuffer.readUInt32BE(0) + 4;
+        savedBuffer = Buffer.concat([savedBuffer, recvBuffer]);
+
+        while(savedBuffer.length >= 4 && savedBuffer.length >= mssgLength()){
+            callback(savedBuffer.subarray(0, mssgLength()));
+            savedBuffer = savedBuffer.subarray(mssgLength());
+            handshake = false;
+        }
+    });
+
+    socket.on('end', ()=>{
+        console.log(`Peer ${peer_ip} disconnected`);
+    });
 }
